5a10bab56624bc4f3fcfef5ea6a06e9480622a56
Felix Amerbauer <felix@amerbauer.at>
2013-05-02 19:01:42 +0200

    wip code and forum working after er diagram change

--- src/main/scala/bi/Importer.scala
+++ src/main/scala/bi/Importer.scala
@@ -2,159 +2,226 @@ package bi
 
 import java.io.File
 import java.io.FileFilter
-import java.nio.file.Files
-import java.nio.file.Path
-import java.nio.file.Paths.get
-import java.nio.file.StandardOpenOption
+import java.sql.DriverManager
+import java.sql.Timestamp
+import java.util.Date
 
 import scala.Array.canBuildFrom
-import scala.io.Codec
-import scala.io.Source
+import scala.Array.fallbackCanBuildFrom
 import scala.xml.Node
 import scala.xml.NodeSeq.seqToNodeSeq
 import scala.xml.XML
 
 import org.joda.time.LocalDateTime
 import org.joda.time.format.DateTimeFormat
+import org.squeryl.PrimitiveTypeMode.__thisDsl
+import org.squeryl.PrimitiveTypeMode.int2ScalarInt
+import org.squeryl.PrimitiveTypeMode.transaction
+import org.squeryl.PrimitiveTypeMode.view2QueryAll
+import org.squeryl.Session
+import org.squeryl.SessionFactory
+import org.squeryl.adapters.MySQLAdapter
 
 import com.typesafe.scalalogging.slf4j.Logging
+
+import bi.MySchema.tCodeUpload
+import bi.MySchema.tForum
+import bi.MySchema.tForumsbereich
+import bi.MySchema.tPosting
+
 import scala.language.implicitConversions
 
+/**
+ * TODO
+ * C:/tools/uni/pentaho/data/Code/5/Data/1/ref301
+ */
+
 object Importer extends App with Logging {
+  val matrnrRegex = """a\d+""".r
   val dtf = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss")
 
   implicit def optionSeqXmlNode2String(osxn: Option[Seq[Node]]): String = osxn.get.text
   implicit def optionSeqXmlNode2Int(osxn: Option[Seq[Node]]): Int = optionSeqXmlNode2String(osxn).toInt
-  implicit def optionSeqXmlNode2LocalDateTime(osxn: Option[Seq[Node]]): LocalDateTime = dtf.parseLocalDateTime(optionSeqXmlNode2String(osxn))
+  implicit def optionSeqXmlNode2Timestamp(osxn: Option[Seq[Node]]): Timestamp = new Timestamp(dtf.parseLocalDateTime(optionSeqXmlNode2String(osxn)).toDate().getTime())
 
+  implicit def localDateTime2Timestamp(ldt: LocalDateTime): Timestamp = new Timestamp(ldt.toDate().getTime())
   val dataDir = new File("C:/tools/uni/pentaho/data")
-  val csvDir = get("csv")
-  logger.info("start")
-  readForum
 
-  def readForum(): (Seq[Forum], Seq[Person]) = {
-    val forumDir = new File(dataDir, "forum")
+  import org.squeryl.SessionFactory
 
-    def getPostings(forumsId: Int, forumsbereichId: Int): Seq[Posting] = {
-      // recursive method
-      def read(node: Node): Posting = {
-        // Data of a single posting
-        val user: String = node.attribute("user")
-        val name: String = node.attribute("name")
-        val id: Int = node.attribute("id")
-        val date: LocalDateTime = node.attribute("date")
-        val betreff = (node \ "subject").text
-        val text = (node \ "text").text
-        // Optional answers
-        val children = node \ "sub" \ "entry" map { e => read(e) }
-        // Build posting
-        Posting(id, date, betreffLaenge = betreff.length(), textLaenge = text.length, children, None)
-      }
-      val file = new File(forumDir, s"$forumsId/Data/$forumsbereichId.xml")
-      if (file.exists()) {
-        val data = XML.loadFile(file)
-        data \ "entry" map { e => read(e) }
-      } else {
-        logger.error(s"No postings for forumsId $forumsId forumsbereichId $forumsbereichId")
-        Seq()
-      }
-    }
-
-    def getForumsbereiche(forumsId: Int): Seq[Forumsbereich] =
-      XML.loadFile(new File(forumDir, s"$forumsId/issues.xml")) \ "entry" map { e =>
-        val id: Int = e.attribute("id")
-        val name: String = e.attribute("what")
-        Forumsbereich(id, name, getPostings(forumsId, id))
-      }
-
-    def getForums: Seq[Forum] = {
-      val descriptions = XML.loadFile(new File(forumDir, "descriptions.xml"))
-      val pattern = """KURS(\d\d)/?u?e?/se(\d\d)""".r
-      descriptions \ "instance" map { e =>
-        val id: Int = e.attribute("id")
-        val pattern(lva, semester) = pattern.findFirstMatchIn(e.text).get
-        Forum(id, Lva(lva, Semester(semester.toInt)), getForumsbereiche(id))
-      }
-    }
-
-    def getPersons(forumsIds: Seq[Int]): Seq[(Int, Seq[Person])] = {
-      def personsForum(forumsId: Int): Seq[Person] = {
-        val data = XML.loadFile(new File(forumDir, s"$forumsId/persons.xml"))
-        data \ "person" map { e =>
-          val id: String = e.attribute("id")
-          val name: String = e.attribute("name")
-          val email: String = e.attribute("email")
-          Person(id = id,
-            name = name,
-            email = email,
-            geschlecht = None,
-            dateOfBirth = None,
-            // differntiate all types
-            personType = { if (id.startsWith("a")) Student else Lecturer })
-        }
-      }
-      forumsIds.map(e => (e, personsForum(e)))
-    }
-
-    def getReadProgress(forumsIds: Seq[Int]) = {
-      forumsIds.map { forumsId =>
-        val dir = new File(forumDir, s"$forumsId/User")
-        val personDirs = dir.listFiles().filter(_.isDirectory())
-
-        for (personDir <- personDirs) yield {
-          println("Reading from " + personDir.getCanonicalPath())
-          val readProgressFilter = new FileFilter {
-            override def accept(file: File) = if ("""\d+""".r.pattern.matcher(file.getName).matches()) true
-            else {
-              logger.error("Invalid read progress file " + file.getCanonicalPath())
-              false
-            }
-
-          }
-          val readProgress = for (readProgressFile <- personDir.listFiles(readProgressFilter)) yield {
-            (readProgressFile.getName, Source.fromFile(readProgressFile).getLines.map(_.toInt))
-          }
-          (personDir.getName(), readProgress)
-        }
-      }
-    }
-
-    val forums = getForums
-    val forumIds = forums.map(_.id)
-
-    val readProgress = getReadProgress(forumIds)
-
-    val personsPerForum = getPersons(forumIds)
-
-    val personsWithDupes = personsPerForum.map(_._2).flatten.sortBy(_.id)
-
-    (forums, /* TODO readProgress*/ personsWithDupes)
+  val dirFilter = new FileFilter {
+    override def accept(file: File) = file.isDirectory()
   }
+  val nonDirFilter = new FileFilter {
+    override def accept(file: File) = !file.isDirectory()
+  }
+
+  def initDb {
+    Class.forName("com.mysql.jdbc.Driver");
+    SessionFactory.concreteFactory = Some(() =>
+      Session.create(
+        DriverManager.getConnection("jdbc:mysql://localhost/uni_business_intelligence?user=bi&password=bi"),
+        new MySQLAdapter))
+  }
+  logger.info("start")
   def code() {
     logger.info("code")
-    val dirFilter = new FileFilter {
-      override def accept(file: File) = file.isDirectory()
+    val matrnrDirFilter = new FileFilter {
+      override def accept(file: File) = file.isDirectory() && matrnrRegex.findFirstIn(file.getName()).isDefined
     }
     val codeDir = new File(dataDir, "code")
     def getInstances: Seq[Int] = codeDir.listFiles(dirFilter).map(_.getName().toInt)
 
     def getUploads(instance: Int): Seq[(String, LocalDateTime)] = {
+      def getLatestCodeArtefact(dir: File): Option[File] = {
+        val files = dir.listFiles(nonDirFilter)
+        val dirs = dir.listFiles(dirFilter)
+        (!files.isEmpty, !dirs.isEmpty) match {
+          case (true, _) => Some(files.sortBy(_.lastModified()).last)
+          case (false, true) =>
+            val subdir = dirs.sortBy(_.lastModified()).last
+            Some(subdir.listFiles(nonDirFilter).sortBy(_.lastModified).last)
+          case _ =>
+            logger.warn("No code artefacts in " + dir.getCanonicalPath())
+            None
+        }
+
+      }
       // e.g. C:/tools/uni/pentaho/data/Code/1/Data/1/a0127319
-      new File(codeDir, s"$instance/Data/1").listFiles(dirFilter).map { matrnrDir =>
-        val currentFile = matrnrDir.listFiles(dirFilter).sortBy(_.lastModified()).last
-        (matrnrDir.getName(),LocalDateTime.now())
+      for {
+        matrnrDir <- new File(codeDir, s"$instance/Data/1").listFiles(matrnrDirFilter)
+        currentFile = getLatestCodeArtefact(matrnrDir)
+        if (currentFile.isDefined)
+      } yield {
+        logger.debug("Checking " + matrnrDir.getCanonicalPath())
+
+        (matrnrDir.getName(), LocalDateTime.fromDateFields(new Date(currentFile.get.lastModified())))
       }
     }
-
+    val data = getInstances.map { e => (e, getUploads(e)) }
+    // persist to db
+    transaction {
+      for {
+        (instanceid, row) <- data
+        (matrnr, lastModified) <- row
+      } {
+        val cd = CodeUpload(id = -1, zeitstempel = lastModified, user_id = matrnr, code_topic_id = instanceid)
+        tCodeUpload.insert(cd)
+      }
+    }
+    println(data)
   }
   def forum() {
     logger.info("forum")
+    logger.info("emptying tables posting, forumsbereich")
+    transaction {
+      tPosting.deleteWhere(_ => 1 === 1)
+      tForumsbereich.deleteWhere(_ => 1 === 1)
+    }
+
+    val forumDir = new File(dataDir, "forum")
+
+    def readAndInsertPostings(forumsId: Long, forumsbereichId: Long, forumsbereichDbid: Long) {
+      // recursive method
+      def process(node: Node, parent_id: Option[Long] = None) {
+        // Data of a single posting
+        val user: String = node.attribute("user")
+        val name: String = node.attribute("name")
+        val id: Int = node.attribute("id")
+        val date: Timestamp = node.attribute("date")
+        val betreff = (node \ "subject").text
+        val text = (node \ "text").text
+        val postingDb = transaction {
+          val postingToInsert = Posting(id = -1, postingid = id, datum = date, betreff_laenge = betreff.length(), text_laenge = text.length(), forumsbereich_id = forumsbereichDbid, parent_id = None)
+          logger.info("Inserting " + postingToInsert)
+          tPosting.insert(postingToInsert)
+        }
+        // Optional answers
+        node \ "sub" \ "entry" foreach { e => process(e, Some(postingDb.id)) }
+      }
+      val file = new File(forumDir, s"$forumsId/Data/$forumsbereichId.xml")
+      if (file.exists()) {
+        val data = XML.loadFile(file)
+        data \ "entry" foreach { e => process(e) }
+      } else {
+        logger.error(s"No postings for forumsId $forumsId forumsbereichId $forumsbereichId")
+      }
+    }
+
+    def getForumsbereiche(forumsId: Long): Seq[Forumsbereich] =
+      XML.loadFile(new File(forumDir, s"$forumsId/issues.xml")) \ "entry" map { e =>
+        val id: Int = e.attribute("id")
+        val name: String = e.attribute("what")
+        val beschreibung: String = (e \ "text").text
+        Forumsbereich(id = -1, bereichsid = id, name = name, beschreibung = beschreibung, forum_id = forumsId)
+      }
+
+//    def getPersons(forumsIds: Seq[Int]): Seq[(Int, Seq[Person])] = {
+//      def personsForum(forumsId: Int): Seq[Person] = {
+//        val data = XML.loadFile(new File(forumDir, s"$forumsId/persons.xml"))
+//        data \ "person" map { e =>
+//          val id: String = e.attribute("id")
+//          val name: String = e.attribute("name")
+//          val email: String = e.attribute("email")
+//          Person(id = id,
+//            name = name,
+//            email = email,
+//            geschlecht = None,
+//            dateOfBirth = None,
+//            // differntiate all types
+//            personType = { if (id.startsWith("a")) Student else Lecturer })
+//        }
+//      }
+//      forumsIds.map(e => (e, personsForum(e)))
+//    }
+//
+//    def getReadProgress(forumsIds: Seq[Int]) = {
+//      forumsIds.map { forumsId =>
+//        val dir = new File(forumDir, s"$forumsId/User")
+//        val personDirs = dir.listFiles().filter(_.isDirectory())
+//
+//        for (personDir <- personDirs) yield {
+//          println("Reading from " + personDir.getCanonicalPath())
+//          val readProgressFilter = new FileFilter {
+//            override def accept(file: File) = if ("""\d+""".r.pattern.matcher(file.getName).matches()) true
+//            else {
+//              logger.error("Invalid read progress file " + file.getCanonicalPath())
+//              false
+//            }
+//          }
+//          val readProgress = for (readProgressFile <- personDir.listFiles(readProgressFilter)) yield {
+//            (readProgressFile.getName, Source.fromFile(readProgressFile).getLines.map(_.toInt))
+//          }
+//          (personDir.getName(), readProgress)
+//        }
+//      }
+//    }
+
+    val forums = transaction { tForum.toArray }
+    val forumsbereiche =
+      transaction {
+        val toInsert = forums.flatMap(e => getForumsbereiche(e.id))
+        logger.info("Inserting\n\t" + toInsert.mkString("-" * 30, "\n\t", "-" * 30))
+        tForumsbereich.insert(toInsert)
+        tForumsbereich.toArray
+      }
+    val posting = forumsbereiche.foreach(e => readAndInsertPostings(forumsId = e.forum_id, forumsbereichId = e.bereichsid, forumsbereichDbid = e.id))
+    //    val forumIds = forums.map(_.id)
+
+    //    val readProgress = getReadProgress(forumIds)
+    //
+    //    val personsPerForum = getPersons(forumIds)
+    //
+    //    val personsWithDupes = personsPerForum.map(_._2).flatten.sortBy(_.id)
+
+    //    (forums, /* TODO readProgress*/ personsWithDupes)
 
   }
+  initDb
   args match {
     case Array("code") => code()
     case Array("forum") => forum()
     case _ => println("Invalid parameters")
   }
-  val (forums, persons) = readForum
+  //  val (forums, persons) = readForum
 }
--- src/main/scala/bi/Model.scala
+++ src/main/scala/bi/Model.scala
@@ -26,115 +26,114 @@ case class Person(
   geschlecht: Option[Sex],
   dateOfBirth: Option[LocalDate],
   personType: PersonType)
-  
 
 case class Lva(name: String, semester: Semester)
 
 case class Semester(val id: Int)
 
 // Forum
-case class Forum(id: Int, lva: Lva, bereiche: Seq[Forumsbereich])
+case class Forum(id: Long, langtext: String, lva_dbid: Long) extends KeyedEntity[Long]
 
-case class Forumsbereich(id: Int, name: String, postings: Seq[Posting]) 
+/*
+CREATE TABLE `forumsbereich` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `bereichsid` int(11) NOT NULL,
+  `name` varchar(64) DEFAULT NULL,
+  `beschreibung` varchar(256) DEFAULT NULL,
+  `forum_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `forumsbereich_unique` (`bereichsid`,`forum_id`),
+  KEY `forumsbereich_forum_idx` (`forum_id`),
+  CONSTRAINT `forumsbereich_forum` FOREIGN KEY (`forum_id`) REFERENCES `forum` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+*/
+
+case class Forumsbereich(id: Long, bereichsid: Long, name: String, beschreibung: String, forum_id: Long) extends KeyedEntity[Long]
 
 object Forumsbereich {
   private val header = s"id${s}name"
 }
 
-case class Posting(id: Int, datum: LocalDateTime, betreffLaenge: Int, textLaenge: Int, sub: Seq[Posting], parent: Option[Posting]) 
+case class Posting(id: Long, postingid: Long, datum: Timestamp, betreff_laenge: Int, text_laenge: Int, forumsbereich_id: Long, parent_id: Option[Long]) extends KeyedEntity[Long]
 
-object Posting {
-  private val header = s"id${s}datum${s}betreffLänge${s}textLänge${s}parent"
-}
-trait MyKeyedEntity[K] extends PersistenceStatus {
+case class CodeTopic(id: Long, name: String, lva_dbid: Int) extends KeyedEntity[Long]
 
-  def dbid: K
-
-  override def hashCode =
-    if(isPersisted)
-      dbid.hashCode
-    else
-      super.hashCode
-
-  override def equals(z: Any):Boolean = {
-    if(z == null)
-      return false
-    val ar = z.asInstanceOf[AnyRef]
-    if(!ar.getClass.isAssignableFrom(this.getClass))
-      false
-    else if(isPersisted)
-      dbid == ar.asInstanceOf[MyKeyedEntity[K]].dbid
-    else
-      super.equals(z)
-  }
-}
-case class CodeTopic(dbid:Long, id:Int, name:String, lva_dbid:Int) extends MyKeyedEntity[Long]
-
-case class CodeUpload(dbid:Long, zeitstempel:Timestamp, user_id:Int, code_topic_id:Int) extends MyKeyedEntity[Long]
+case class CodeUpload(id: Long, zeitstempel: Timestamp, user_id: String, code_topic_id: Int) extends KeyedEntity[Long]
 
 object MySchema extends Schema {
   val tCodeTopic = table[CodeTopic]("code_topic")
   on(tCodeTopic)(g => declare(
-    g.dbid is (primaryKey, autoIncremented)))
+    g.id is (primaryKey)))
 
   val tCodeUpload = table[CodeUpload]("code_upload")
   on(tCodeUpload)(g => declare(
-    g.dbid is (primaryKey, autoIncremented)))
+    g.id is (primaryKey, autoIncremented)))
 
+  val tForum = table[Forum]("forum")
+  on(tForum)(g => declare(
+    g.id is (primaryKey)))
 
-//  // ManyToMany Event - Membership
-//  class EventMembership(val eventId: Long, val membershipId: Long) extends KeyedEntity[CompositeKey2[Long, Long]] {
-//    override def id = compositeKey(eventId, membershipId)
-//  }
-//  val eventsMemberships =
-//    manyToManyRelation(tEvent, tMembership, "event_membership").
-//      via[EventMembership]((event, membership, cs) => (cs.eventId === event.id, membership.id === cs.membershipId))
-//
-//  // ManyToMany User - Games
-//  class UserGame(val userId: Long, val gameId: Long, var signedIn: SignedInEnum.SignedInEnum, var comment: String) extends KeyedEntity[CompositeKey2[Long, Long]] {
-//    //    def this=this(0,0,0,SignedInEnum.Unknown)
-//    def this() = this(0L, 0L, SignedInEnum.Unknown, "")
-//
-//    override def id = compositeKey(userId, gameId)
-//    def signedInEnum = signedIn
-//    override def toString = userId + "/" + gameId + "/" + signedInEnum
-//  }
-//  val usersGames =
-//    manyToManyRelation(tUser, tGame, "user_game").
-//      via[UserGame]((user, game, cs) => (cs.userId === user.id, game.id === cs.gameId))
-//
-//  // ManyToMany User - Memberships
-//  class UserMembership(val userId: Long, val membershipId: Long) extends KeyedEntity[CompositeKey2[Long, Long]] {
-//    override def id = compositeKey(userId, membershipId)
-//  }
-//  val usersMemberships =
-//    manyToManyRelation(tUser, tMembership, "user_membership").
-//      via[UserMembership]((user, membership, cs) => (cs.userId === user.id, membership.id === cs.membershipId))
-//
-//  //   OneToMany Membership - Game
-//  val gameMemberships = oneToManyRelation(tMembership, tGame).via((m, g) => g.membershipId === m.id)
-//
-//  //   OneToMany User - Game (launderer)
-//  val gameLaunderers = oneToManyRelation(tUser, tGame).via((u, g) => g.laundererId === u.id)
-//
-//  // Joins
-//  def getEventsMemberships =
-//    from(tEvent, tMembership, eventsMemberships)((e, m, em) =>
-//      where(e.id === em.eventId and m.id === em.membershipId)
-//        select (e, m) orderBy (e.start))
-//  def getEventsMemberships(eventsAfter: Timestamp) =
-//    from(tEvent, tMembership, eventsMemberships)((e, m, em) =>
-//      where(e.id === em.eventId and m.id === em.membershipId and (e.start >= eventsAfter))
-//        select (e, m) orderBy (e.start))
-//
-//  def getEvent(id: Long) = tEvent.where(_.id === id).single
-//
-//  def getEventsWithMemberships = getEventsMemberships.asTree.map { e => e._1.memberships2 = e._2.map(_.name); e._1 }.toSeq.sortBy(_.start.getTime)
-//
-//  def getEventsWithMemberships(eventsAfter: Timestamp) = getEventsMemberships(eventsAfter).asTree.map { e => e._1.memberships2 = e._2.map(_.name); e._1 }.toSeq.sortBy(_.start.getTime)
-//
-//
-//  def allUsers = from(tUser)(select(_))
-//
-//  val allTables = Seq(usersMemberships, usersGames, tGame, tUser, eventsMemberships, tEvent, newsMemberships, tNews, tMembership)
+  val tForumsbereich = table[Forumsbereich]("forumsbereich")
+  on(tForumsbereich)(g => declare(
+    g.id is (primaryKey, autoIncremented)))
+
+  val tPosting = table[Posting]("posting")
+  on(tPosting)(g => declare(
+    g.id is (primaryKey, autoIncremented)))
+
+  //  // ManyToMany Event - Membership
+  //  class EventMembership(val eventId: Long, val membershipId: Long) extends KeyedEntity[CompositeKey2[Long, Long]] {
+  //    override def id = compositeKey(eventId, membershipId)
+  //  }
+  //  val eventsMemberships =
+  //    manyToManyRelation(tEvent, tMembership, "event_membership").
+  //      via[EventMembership]((event, membership, cs) => (cs.eventId === event.id, membership.id === cs.membershipId))
+  //
+  //  // ManyToMany User - Games
+  //  class UserGame(val userId: Long, val gameId: Long, var signedIn: SignedInEnum.SignedInEnum, var comment: String) extends KeyedEntity[CompositeKey2[Long, Long]] {
+  //    //    def this=this(0,0,0,SignedInEnum.Unknown)
+  //    def this() = this(0L, 0L, SignedInEnum.Unknown, "")
+  //
+  //    override def id = compositeKey(userId, gameId)
+  //    def signedInEnum = signedIn
+  //    override def toString = userId + "/" + gameId + "/" + signedInEnum
+  //  }
+  //  val usersGames =
+  //    manyToManyRelation(tUser, tGame, "user_game").
+  //      via[UserGame]((user, game, cs) => (cs.userId === user.id, game.id === cs.gameId))
+  //
+  //  // ManyToMany User - Memberships
+  //  class UserMembership(val userId: Long, val membershipId: Long) extends KeyedEntity[CompositeKey2[Long, Long]] {
+  //    override def id = compositeKey(userId, membershipId)
+  //  }
+  //  val usersMemberships =
+  //    manyToManyRelation(tUser, tMembership, "user_membership").
+  //      via[UserMembership]((user, membership, cs) => (cs.userId === user.id, membership.id === cs.membershipId))
+  //
+  //  //   OneToMany Membership - Game
+  //  val gameMemberships = oneToManyRelation(tMembership, tGame).via((m, g) => g.membershipId === m.id)
+  //
+  //  //   OneToMany User - Game (launderer)
+  //  val gameLaunderers = oneToManyRelation(tUser, tGame).via((u, g) => g.laundererId === u.id)
+  //
+  //  // Joins
+  //  def getEventsMemberships =
+  //    from(tEvent, tMembership, eventsMemberships)((e, m, em) =>
+  //      where(e.id === em.eventId and m.id === em.membershipId)
+  //        select (e, m) orderBy (e.start))
+  //  def getEventsMemberships(eventsAfter: Timestamp) =
+  //    from(tEvent, tMembership, eventsMemberships)((e, m, em) =>
+  //      where(e.id === em.eventId and m.id === em.membershipId and (e.start >= eventsAfter))
+  //        select (e, m) orderBy (e.start))
+  //
+  //  def getEvent(id: Long) = tEvent.where(_.id === id).single
+  //
+  //  def getEventsWithMemberships = getEventsMemberships.asTree.map { e => e._1.memberships2 = e._2.map(_.name); e._1 }.toSeq.sortBy(_.start.getTime)
+  //
+  //  def getEventsWithMemberships(eventsAfter: Timestamp) = getEventsMemberships(eventsAfter).asTree.map { e => e._1.memberships2 = e._2.map(_.name); e._1 }.toSeq.sortBy(_.start.getTime)
+  //
+  //
+  //  def allUsers = from(tUser)(select(_))
+  //
+  //  val allTables = Seq(usersMemberships, usersGames, tGame, tUser, eventsMemberships, tEvent, newsMemberships, tNews, tMembership)
 }
